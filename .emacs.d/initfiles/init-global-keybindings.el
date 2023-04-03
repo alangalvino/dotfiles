7e3b5353bca7bce60d021c15b3a9a4cdd6331d56
Alan <1418294+alangalvino@users.noreply.github.com>
2023-04-03 18:06:41 -0300

    Remove toggle-frame-fullscreen from global keybindings

--- .emacs.d/initfiles/init-global-keybindings.el
+++ .emacs.d/initfiles/init-global-keybindings.el
@@ -5,6 +5,5 @@
 (global-set-key (kbd "C-b _") 'split-window-vertically)
 (global-set-key (kbd "C-b -") 'split-window-vertically)
 (global-set-key (kbd "C-b |") 'split-window-horizontally)
-(global-set-key (kbd "C-b F") 'toggle-frame-fullscreen)
 
 (provide 'init-global-keybindings)
