3495373650e8f78bf1b29feb1bc43377dd3d060a
Alan <1418294+alangalvino@users.noreply.github.com>
2023-04-07 11:08:28 -0300

    Fix evil (org & slime) and global keybindings

--- .emacs.d/initfiles/init-evil.el
+++ .emacs.d/initfiles/init-evil.el
@@ -5,20 +5,10 @@
   :ensure t
   :init
   (setq evil-want-keybinding nil)
-  (evil-set-undo-system 'undo-redo)
-  :hook
-  (('org-mode . (lambda ()
-                  (define-key evil-normal-state-map (kbd "C-<return>") '+org/insert-item-below)
-                  (define-key evil-insert-state-map (kbd "C-<return>") '+org/insert-item-below)
-                  (define-key evil-visual-state-map (kbd "C-<return>") '+org/insert-item-below)
-                  (define-key evil-motion-state-map (kbd "C-<return>") '+org/insert-item-below)))
-   ('slime-repl-mode . (lambda ()
-                         (define-key evil-normal-state-map (kbd "C-r") 'helm-slime-repl-history)
-                         (define-key evil-normal-state-map (kbd "C-m") 'slime-repl-return)
-                         (define-key evil-normal-state-map (kbd "M-.") 'slime-edit-definition))))
   :config
   (evil-mode t)
-  
+  (evil-set-undo-system 'undo-redo)
+
   ;; Repeat search with 'S' and 's'
   (define-key evil-normal-state-map "s" 'evil-search-next)
   (define-key evil-normal-state-map "S" 'evil-search-previous)
@@ -36,14 +26,14 @@
   ;; remap change word to 'C' (capital 'c')
   (define-key evil-visual-state-map "c" 'evil-change)
   (define-key evil-normal-state-map "C" 'evil-change)
-  
+
   ;; remap movement keys to htcn (dvorak)
   (define-key evil-normal-state-map "c" 'evil-previous-line)
   (define-key evil-visual-state-map "t" 'evil-next-line)
   (define-key evil-normal-state-map "t" 'evil-next-line)
   (define-key evil-visual-state-map "n" 'evil-forward-char)
   (define-key evil-normal-state-map "n" 'evil-forward-char)
-  
+
   ;;  Ctrl + g as ESC
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
 
@@ -63,7 +53,6 @@
   (define-key evil-normal-state-map (kbd "M-.") 'xref-find-definitions)
   (define-key evil-visual-state-map (kbd "M-.") 'xref-find-definitions)
 
-  (define-key evil-normal-state-map "-" 'comment-line)
   (define-key evil-visual-state-map "-" 'comment-or-uncomment-region))
 
 (use-package evil-leader
@@ -86,9 +75,9 @@
     "/"   'helm-occur
     ;; t for terminal
     "t"   'vterm-toggle
-    ;; e for editing 
+    ;; e for editing
     "e"   'darkroom-tentative-mode
-    ;; d for dired 
+    ;; d for dired
     "d"   'treemacs-display-current-project-exclusively
     ;; s for slime
     "s"   'slime
@@ -106,6 +95,27 @@
   :delight
   :ensure t
   :config
-  (evil-collection-init))
+  (evil-collection-init)
+  (evil-collection-define-key 'normal 'slime-mode-map
+    (kbd "M-.") 'slime-edit-definition)
+  (evil-collection-define-key 'normal 'slime-repl-mode-map
+    (kbd "C-r") 'helm-slime-repl-history
+    (kbd "C-m") 'slime-repl-return)
+  (evil-collection-define-key 'normal 'org-mode-map
+    "RET"        '+org/dwim-at-point
+    [return]     '+org/dwim-at-point
+    "C-S-RET"    '+org/insert-item-above
+    [C-S-return] '+org/insert-item-above
+    "C-RET"      '+org/insert-item-below
+    [C-return]   '+org/insert-item-below)
+  (evil-collection-define-key 'insert 'org-mode-map
+    "RET"         '+org/return
+    [return]     '+org/return
+    "S-RET"      '+org/shift-return
+    [S-return]   '+org/shift-return
+    "C-S-RET"    '+org/insert-item-above
+    [C-S-return] '+org/insert-item-above
+    "C-RET"      '+org/insert-item-below
+    [C-return]   '+org-insert-item-below))
 
 (provide 'init-evil)
--- .emacs.d/initfiles/init-global-keybindings.el
+++ .emacs.d/initfiles/init-global-keybindings.el
@@ -2,8 +2,5 @@
 
 (global-set-key (kbd "C-x h") 'help-command)
 (global-set-key (kbd "M-q")   'set-justification-full)
-(global-set-key (kbd "C-b _") 'split-window-vertically)
-(global-set-key (kbd "C-b -") 'split-window-vertically)
-(global-set-key (kbd "C-b |") 'split-window-horizontally)
 
 (provide 'init-global-keybindings)
